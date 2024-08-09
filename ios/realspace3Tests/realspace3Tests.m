499d8841d3b700b49f1b92f0518377a6aee7c3d7
joao neiva <jg.neiva@gmail.com>
2024-08-09 10:54:51 +0000

    updated codemagic.yaml

--- ios/realspace3Tests/realspace3Tests.m
+++ ios/realspace3Tests/realspace3Tests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface tempProjectTests : XCTestCase
+@interface realspace3Tests : XCTestCase
 
 @end
 
-@implementation tempProjectTests
+@implementation realspace3Tests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
